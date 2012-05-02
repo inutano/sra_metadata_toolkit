3ca50b48caeee9ebb12b0c2fe983ba41f5597497
tazro inutano ohta <inutano@gmail.com>
2012-05-02 20:47:19 +0900

    add metadata parser part, how to bring url parameter to method call?

--- app.rb
+++ app.rb
@@ -10,14 +10,41 @@ def obj_gen(query)
   case
   when $2 == "A"
     SubmissionID.new(query)
+
   when $2 == "P"
     StudyID.new(query)
+
   when $2 == "X"
     ExperimentID.new(query)
+
   when $2 == "S"
     SampleID.new(query)
+
   when $2 == "R"
     RunID.new(query)
+  end
+end
+
+def get_parser(id)
+  obj = obj_gen(id)
+  submission_id = obj.submission
+  f_prefix = "#{File.expand_path(File.dirname(__FILE__))}/latest/#{submission_id}/#{submission_id}"
+  obj_class = obj.class
+  case
+  when obj.class == SubmissionID
+    SubmissionParser.new(id, f_prefix + ".submission.xml")
+
+  when obj.class == StudyID
+    StudyParser.new(id, f_prefix + ".study.xml")
+
+  when obj.class == ExperimentID
+    ExperimentParser.new(id, f_prefix + ".experiment.xml")
+
+  when obj.class == SampleID
+    SampleParser.new(id, f_prefix + ".sample.xml")
+
+  when obj.class == RunID
+    RunParser.new(id, f_prefix + ".run.xml")
   end
 end
 
@@ -33,12 +60,16 @@ get "/*.to_*" do
     result = case
              when dest == "submission"
                obj.submission
+
              when dest == "study"
                obj.study
+
              when dest == "experiment"
                obj.experiment
+
              when dest == "sample"
                obj.sample
+
              when dest == "run"
                obj.run
              end
@@ -50,4 +81,14 @@ get "/*.to_*" do
   else
     "invalid: #{origin}"
   end
+end
+
+
+get "/*.*" do
+  id = params[:splat][0]
+  kind = params[:splat][1]
+
+  xml = get_xml(id)
+  metadata_parser = get_parser(id)
+  
 end
