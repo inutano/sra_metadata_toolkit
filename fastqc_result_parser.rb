a46c66691eaac01f5b948c6cec50b5b571c84449
tazro inutano ohta <inutano@gmail.com>
2012-05-21 15:47:58 +0900

    defined basic stat

--- fastqc_result_parser.rb
+++ fastqc_result_parser.rb
@@ -15,6 +15,42 @@ class FastQCparser
     base =~ /Filename\t(.+?)\t/m
     $1
   end
+  
+  def file_type
+    base = self.basic_statistics
+    base =~ /File type\t(.+?)\t/m
+    $1
+  end
+  
+  def encoding
+    base = self.basic_statistics
+    base =~ /Encoding\t(.+?)\t/m
+    $1
+  end
+  
+  def total_sequences
+    base = self.basic_statistics
+    base =~ /Total Sequences\t(.+?)\t/m
+    $1
+  end
+  
+  def filtered_sequences
+    base = self.basic_statistics
+    base =~ /Filtered Sequences\t(.+?)\t/m
+    $1
+  end
+  
+  def sequence_length
+    base = self.basic_statistics
+    base =~ /Sequence length\t(.+?)\t/m
+    $1
+  end
+  
+  def percent_gc
+    base = self.basic_statistics
+    base =~ /\%GC\t(.+?)\t/m
+    $1
+  end
 end
 
 if __FILE__ == $0
@@ -23,4 +59,9 @@ if __FILE__ == $0
   file = "/Volumes/Macintosh HD 4/fastqc/SRR001001/SRR001001_1_fastqc/fastqc_data.txt"
   f = FastQCparser.new(file)
   ap f.filename
+  ap f.file_type
+  ap f.encoding
+  ap f.total_sequences
+  ap f.sequence_length
+  ap f.percent_gc 
 end
