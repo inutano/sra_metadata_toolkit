83c22662631aef847616715bd3a9da14077553cd
tazro inutano ohta <inutano@gmail.com>
2013-06-13 16:17:59 +0900

    qcparser fully refactorred

--- fastqc_result_parser.rb
+++ fastqc_result_parser.rb
@@ -1,77 +1,78 @@
 # -*- coding: utf-8 -*-
 
 class FastQCParser
-  def initialize(fastqc_data_path)
-    @txt = open(fastqc_data_path).read
-    @txt =~ /(>>Basic Statistics.*?)>>END_MODULE/m
-    @basic_stat = $1
+  def initialize(fpath)
+    txt = open(fpath).read
+    @obj = parse(txt)
+    @base = self.basic_statistics
+  end
+  
+  def parse(txt)
+    modules = txt.split(">>END_MODULE\n")
+    modules.map do |node|
+      lines = node.split("\n")
+      rm_header = lines.map do |line|
+        if line !~ /^\#/ || line =~ /^#Total Duplicate Percentage/
+          line.split("\t")
+        end
+      end
+      rm_header.compact
+    end
   end
   
   def basic_statistics
-    @txt =~ /(>>Basic Statistics.*?)>>END_MODULE/m
-    $1
+    Hash[*@obj.select{|a| a.first.first == ">>Basic Statistics" }.flatten]
   end
   
   def filename
-    @basic_stat =~ /Filename\t(.+?)\t/m
-    $1
+    @base["Filename"]
   end
   
   def file_type
-    @basic_stat =~ /File type\t(.+?)\t/m
-    $1
+    @base["File type"]
   end
   
   def encoding
-    @basic_stat =~ /Encoding\t(.+?)\t/m
-    $1
+    @base["Encoding"]
   end
   
   def total_sequences
-    @basic_stat =~ /Total Sequences\t(.+?)\t/m
-    $1.to_i
+    @base["Total Sequences"].to_i
   end
   
   def filtered_sequences
-    @basic_stat =~ /Filtered Sequences\t(.+?)\t/m
-    $1.to_i
+    @base["Filtered Sequences"].to_i
   end
   
   def sequence_length
-    @basic_stat =~ /Sequence length\t(.+?)\t/m
-    $1
+    @base["Sequence length"]
   end
   
   def min_length
-    len = self.sequence_length
-    if len =~ /\d-\d/
-      len.sub(/-\d+$/,"").to_i
+    l = @base["Sequence length"]
+    if l =~ /\d-\d/
+      l.sub(/-\d+$/,"").to_i
     else
-      len.to_i
+      l.to_i
     end
   end
-
+  
   def max_length
-    len = self.sequence_length
-    if len =~ /\d-\d/
-      len.sub(/^\d+-/,"").to_i
+    l = @base["Sequence length"]
+    if l =~ /\d-\d/
+      l.sub(/^\d+-/,"").to_i
     else
-      len.to_i
+      l.to_i
     end
   end
   
   def percent_gc
-    @basic_stat =~ /\%GC\t(.+?)\t/m
-    $1.to_f
+    @base["%GC"].to_i
   end
   
   def per_base_sequence_quality
-    # returns 2d array
-    # column: base, mean, median, lower quartile, upper quartile, 10th Percentile, 90th Percentile
-    @txt =~ /(>>Per base sequence quality.+?)>>END_MODULE/m
-    base = $1
-    mline = base.split("\n")
-    mline.select{|l| l =~ /^\d/ }.map{|c| c.split("\t") }
+    node = @obj.select{|a| a.first.first == ">>Per base sequence quality" }
+    node.first.select{|n| n.first != ">>Per base sequence quality" }
   end
   
   def total_mean_sequence_qual
@@ -82,53 +83,33 @@ class FastQCParser
   
   def normalized_phred_score
     per_base = self.per_base_sequence_quality
-    per_base_median = per_base.map{|c| c[2].to_f }
-    per_base_median.reduce(:+) / per_base_median.length
+    v = per_base.map{|c| c[2].to_f }
+    v.reduce(:+) / v.size
   end
   
   def per_sequence_quality_scores
-    # returns 2d array
-    # column: quality, count
-    @txt =~ /(>>Per sequence quality scores.+?)>>END_MODULE/m
-    base = $1
-    mline = base.split("\n")
-    mline.select{|l| l =~ /^\d/ }.map{|c| c.split("\t") }
+    node = @obj.select{|a| a.first.first == ">>Per sequence quality scores" }
+    node.first.select{|n| n.first != ">>Per sequence quality scores" }
   end
   
   def per_base_sequence_content
-    # returns 2d array
-    # column: base position, % of base G, A, T, and C
-    @txt =~ /(>>Per base sequence content.+?)>>END_MODULE/m
-    base = $1
-    mline = base.split("\n")
-    mline.select{|l| l =~ /^\d/ }.map{|c| c.split("\t") }
+    node = @obj.select{|a| a.first.first == ">>Per base sequence content" }
+    node.first.select{|n| n.first != ">>Per base sequence content" }
   end
   
   def per_base_gc_content
-    # returns 2d array
-    # column: base position, %GC
-    @txt =~ /(>>Per base GC content.+?)>>END_MODULE/m
-    base = $1
-    mline = base.split("\n")
-    mline.select{|l| l =~ /^\d/ }.map{|c| c.split("\t") }    
+    node = @obj.select{|a| a.first.first == ">>Per base GC content" }
+    node.first.select{|n| n.first != ">>Per base GC content" }
   end
   
   def per_sequence_gc_content
-    # returns 2d array
-    # column: GC content, read count
-    @txt =~ /(>>Per sequence GC content.+?)>>END_MODULE/m
-    base = $1
-    mline = base.split("\n")
-    mline.select{|l| l =~ /^\d/ }.map{|c| c.split("\t") }
+    node = @obj.select{|a| a.first.first == ">>Per sequence GC content" }
+    node.first.select{|n| n.first != ">>Per sequence GC content" }
   end
   
   def per_base_n_content
-    # returns 2d array
-    # column: base, n-count
-    @txt =~ /(>>Per base N content.+?)>>END_MODULE/m
-    base = $1
-    mline = base.split("\n")
-    mline.select{|l| l =~ /^\d/ }.map{|c| c.split("\t") }
+    node = @obj.select{|a| a.first.first == ">>Per base N content" }
+    node.first.select{|n| n.first != ">>Per base N content" }
   end
   
   def total_n_content
@@ -138,23 +119,19 @@ class FastQCParser
   end
   
   def sequence_length_distribution
-    # returns 2d array
-    # column: length, count
-    @txt =~ /(>>Sequence Length Distribution.+?)>>END_MODULE/m
-    base = $1
-    mline = base.split("\n")
-    mline.select{|l| l =~ /^\d/ }.map{|c| c.split("\t") }
+    node = @obj.select{|a| a.first.first == ">>Sequence Length Distribution" }
+    node.first.select{|n| n.first != ">>Sequence Length Distribution" }
   end
   
   def mean_sequence_length
     distribution = self.sequence_length_distribution
     sum = distribution.map do |length_count|
       length = length_count[0]
-      count = length_count[1].to_f
+      count = length_count[1].to_i
       if length =~ /\d-\d/
-        f = length.sub(/-\d+$/,"").to_f
-        b = length.sub(/^\d+-/,"").to_f
-        mean = (f + b) / 2 + 1
+        f = length.sub(/-\d+$/,"").to_i
+        b = length.sub(/^\d+-/,"").to_i
+        mean = (f + b) / 2
         mean * count
       else
         length.to_i * count
@@ -189,35 +166,23 @@ class FastQCParser
   end
   
   def sequence_duplication_levels
-    # returns 2d array
-    # column: duplication level, relative count
-    @txt =~ /(>>Sequence Duplication Levels.+?)>>END_MODULE/m
-    base = $1
-    mline = base.split("\n")
-    mline.select{|l| l =~ /^\d/ }.map{|c| c.split("\t") }
+    node = @obj.select{|a| a.first.first == ">>Sequence Duplication Levels" }
+    node.first.select{|n| n.first != ">>Sequence Duplication Levels" && n.first != "\#Total Duplicate Percentage" }
   end
   
   def total_duplicate_percentage
-    line = @txt.split("\n").select{|l| l =~ /^.Total Duplicate Percentage/ }.first
-    line.split("\t")[1].to_f
+    node = @obj.select{|a| a.first.first == ">>Sequence Duplication Levels" }
+    node.first.select{|n| n.first == "\#Total Duplicate Percentage" }.flatten[1].to_f
   end
   
   def overrepresented_sequences
-    # returns 2d array
-    # column: sequence, count, %, possible source
-    @txt =~ /(>>Overrepresented sequences.+?)>>END_MODULE/m
-    base = $1
-    mline = base.split("\n")
-    mline.select{|l| l =~ /^(A|T|G|C)/ }.map{|c| c.split("\t") }
+    node = @obj.select{|a| a.first.first == ">>Overrepresented sequences" }
+    node.first.select{|n| n.first != ">>Overrepresented sequences" }
   end
   
-  def kmer_content
-    # returns 2d array
-    # column: sequence, count, obs/exp overall, obs/exp max, max obs/exp position
-    @txt =~ /(>>Kmer Content.+?)>>END_MODULE/m
-    base = $1
-    mline = base.split("\n")
-    mline.select{|l| l =~ /^(A|T|G|C)/ }.map{|c| c.split("\t") }    
+  def  kmer_content
+    node = @obj.select{|a| a.first.first == ">>Kmer Content" }
+    node.first.select{|n| n.first != ">>Kmer Content" }
   end
   
   def all
@@ -237,7 +202,15 @@ class FastQCParser
       sequence_length_distribution: self.sequence_length_distribution,
       sequence_duplication_levels: self.sequence_duplication_levels,
       overrepresented_sequences: self.overrepresented_sequences,
-      kmer_content: self.kmer_content }
+      kmer_content: self.kmer_content,
+      min_length: self.min_length,
+      max_length: self.max_length,
+      total_mean_sequence_qual: self.total_mean_sequence_qual,
+      normalized_phred_score: self.normalized_phred_score,
+      total_n_content: self.total_n_content,
+      mean_sequence_length: self.mean_sequence_length,
+      median_sequence_length: self.median_sequence_length,
+      total_duplicate_percentage: self.total_duplicate_percentage }
   end
 end
 
@@ -248,27 +221,4 @@ if __FILE__ == $0
   #file = "/Users/inutano/project/statistics_sra/fastqc_data/SRR515/SRR515734/SRR515734_1_fastqc/fastqc_data.txt"
   f = FastQCParser.new(file)
   ap f.all
-  
-  ap "total mean"
-  ap f.total_mean_sequence_qual
-  ap "normalized phred"
-  ap f.normalized_phred_score
-  ap "total n content"
-  ap f.total_n_content
-  ap "total duplication percentage"
-  ap f.total_duplicate_percentage
-  
-  ap "min length"
-  ap f.min_length
-  ap "max length"
-  ap f.max_length
-  
-  ap "total count on read length distribution"
-  ap f.sequence_length_distribution.map{|n| n[1].to_i }.reduce(:+)
-  
-  ap "average sequence length"
-  ap f.mean_sequence_length
-  
-  ap "median sequence length"
-  ap f.median_sequence_length
 end
