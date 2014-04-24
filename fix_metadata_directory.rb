6d88f826634f699ab981e441f59150dd311964c3
tazro inutano ohta <inutano@gmail.com>
2014-04-24 11:04:47 +0900

    fixed a wrong variable name

--- fix_metadata_directory.rb
+++ fix_metadata_directory.rb
@@ -10,7 +10,7 @@ if __FILE__ == $0
   prefix = origin_dirs.map{|f| f.gsub(/...$/,"") }.uniq
   prefix.each do |pfx|
     moveto = File.join(origin, pfx)
-    files_matched = origin_dir.select{|f| f.gsub(/\d{3}$/,"") == pfx }
+    files_matched = origin_dirs.select{|f| f.gsub(/\d{3}$/,"") == pfx }
     paths_matched = files_matched.map{|f| File.join(origin, f) }
     FileUtils.mkdir(moveto)
     FileUtils.mv(paths_matched, moveto)
